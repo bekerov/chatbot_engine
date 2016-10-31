e98e3546a9b5e1feb7d775c70302db90b3aab5b3
Sung-ju Kim <goddoe2@gmail.com>
2016-10-31 15:02:24 +0900

    update named entity recognizer

--- src/modules/named_entity_recognizer/modules/city_recognizer.py
+++ src/modules/named_entity_recognizer/modules/city_recognizer.py
@@ -3,7 +3,7 @@ def find_city(query):
 
     city_list = ["서울","대구","인천","부산","도쿄", "샌프란시스코","뉴욕"]
 
-    target_city = "서울"
+    target_city = None
 
     for city in city_list:
         if city in query:
